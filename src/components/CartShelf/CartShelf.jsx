fdf5feda6fa9e9952900ae112627b5af2c963134
Amira Deuraseh <amiradeu.x@gmail.com>
2025-01-03 11:52:51 +0800

    Build/upgrade packages (#10)
    * Upgrade > react-router v7
    
    * Upgrade > new-component v6
    
    * Remove vercel package

--- src/components/CartShelf/CartShelf.jsx
+++ src/components/CartShelf/CartShelf.jsx
@@ -1,6 +1,6 @@
 import React, { memo, useContext, useRef } from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router'
 import { X, ArrowRight } from 'react-feather'
 
 import { CartContext } from '../CartProvider'
--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,4 +1,4 @@
-import { useRouteError } from 'react-router-dom'
+import { useRouteError } from 'react-router'
 import styled from 'styled-components'
 
 function Error() {
